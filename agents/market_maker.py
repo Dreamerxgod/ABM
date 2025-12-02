341441bc2175bc829c19b2173f90fd8f983018be
Dreamerxgod <forgot_fire@mail.ru>
2025-12-02 13:40:29 +0300

    random from utils

--- agents/market_maker.py
+++ agents/market_maker.py
@@ -1,8 +1,9 @@
 from agents.base_agent import Agent
-import random
+import config as cfg
+from utils import random_utils as ru
 
 class MarketMaker(Agent):
-    def __init__(self, id, spread=0.05):
+    def __init__(self, id, spread=cfg.MARKET_MAKER_SPREAD):
         super().__init__(id)
         self.spread = spread
 
@@ -10,8 +11,8 @@ class MarketMaker(Agent):
         mid_price = market_state['mid_price']
         bid_price = mid_price * (1 - self.spread/2)
         ask_price = mid_price * (1 + self.spread/2)
-        qty_bid = random.randint(1, 5)
-        qty_ask = random.randint(1, 5)
+        qty_bid = ru.randint(1, 5)
+        qty_ask = ru.randint(1, 5)
         return [
             {'agent_id': self.id, 'side': 'buy', 'price': bid_price, 'qty': qty_bid, 'type': 'limit'},
             {'agent_id': self.id, 'side': 'sell', 'price': ask_price, 'qty': qty_ask, 'type': 'limit'}
