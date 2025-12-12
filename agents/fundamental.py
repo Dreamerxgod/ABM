51406ac8a3f4c8a2f7fe872f5fc699e587fc8f67
Dreamerxgod <forgot_fire@mail.ru>
2025-12-12 17:11:08 +0300

    probability of an order is added

--- agents/fundamental.py
+++ agents/fundamental.py
@@ -1,18 +1,24 @@
 from agents.base_agent import Agent
 import config as cfg
+import random
 
 class FundamentalTrader(Agent):
-    def __init__(self, id, fundamental_price=100, aggressiveness=cfg.FUNDAMENTAL_TRADER_AGGRESSIVENESS):
+    def __init__(self, id, fundamental_price=100, aggressiveness=cfg.FUNDAMENTAL_TRADER_AGGRESSIVENESS, order_prob = cfg.FUNDAMENTAL_TRADER_ORDER_PROB):
         super().__init__(id)
         self.fundamental_price = fundamental_price
         self.aggressiveness = aggressiveness
+        self.order_prob = order_prob
+
 
     def act(self, market_state):
         mid = market_state['mid_price']
         self.fundamental_price = market_state['fundamental_price']
 
-        deviation = self.fundamental_price - mid  # если положительное — цена ниже фундаментала
+        if random.random() > self.order_prob:
+            return []
+
+        deviation = self.fundamental_price - mid
         side = 'buy' if deviation > 0 else 'sell'
-        price = mid + deviation * 0.5  # корректируем цену, чтобы немного подтолкнуть рынок
+        price = mid + deviation * 0.5
         qty = max(1, int(abs(deviation) / self.aggressiveness))
         return [{'agent_id': self.id, 'side': side, 'price': price, 'qty': qty}]
